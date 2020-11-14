ea5777bfb612795301eaec63d7076a1e8382cf88
adangu814 <45112087+adangu814@users.noreply.github.com>
2020-11-14 17:09:47 -0500

    Create MenuItems.js

---
+++ src/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Create a new entry!',
+        url: '/EditForm',
+        cName: 'nav-links'
+    },
+]
