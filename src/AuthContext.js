874790f272b4bbe4e39e3bd4b93b23e7aacb40dd
adangu814 <45112087+adangu814@users.noreply.github.com>
2020-11-14 17:07:15 -0500

    Create AuthContext.js

---
+++ src/AuthContext.js
@@ -0,0 +1,99 @@
+import React, { useContext, useState, useEffect } from "react"
+import { auth } from "./fire"
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  async function signup(email, password) {
+    if ((validateEmail(email) && validateEmail_2(email)) !== true) {
+      alert("Error: Invalid email");
+      return null;
+    } else {
+      auth.createUserWithEmailAndPassword(email, password)
+      const user = auth.currentUser;
+      await user.sendEmailVerification();
+    }
+
+  }
+
+  function validateEmail(emailAddress) {
+    var regularExpression = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))){2,6}$/i;
+    return regularExpression.test(emailAddress);
+  }
+
+  function validateEmail_2(emailAddress) {
+    if ((emailAddress.includes('@ad.unc.edu') || emailAddress.includes('@live.unc.edu')) === false) {
+      return false
+    } else {
+      var index = 0;
+      for (let i = 0; i < emailAddress.length; i++) {
+        if (emailAddress[i] === '@') {
+          index = i;
+          break;
+        }
+      }
+
+      var new_string = emailAddress.slice(index, emailAddress.length)
+      var valid_domain_1 = '@ad.unc.edu'
+      var valid_domain_2 = '@live.unc.edu'
+      for (let i = 0; i < new_string.length; i++) {
+        if ((new_string[i] !== valid_domain_1[i]) && (new_string[i] !== valid_domain_2[i])) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+  
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+  }
+
+  function logout() {
+    return auth.signOut()
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email)
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
