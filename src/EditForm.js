a96ca7f275cfb30309d21a14f3f845eafba2d25b
adangu814 <45112087+adangu814@users.noreply.github.com>
2020-11-19 17:56:47 -0500

    Update EditForm.js

--- src/EditForm.js
+++ src/EditForm.js
@@ -2,6 +2,7 @@ import firebase from "firebase";
 import React, { useState } from "react"
 import { useHistory } from "react-router-dom";
 import { useAuth } from "./AuthContext"
+import "./EditForm.css"
 
 export default function EditForm() {
     const { currentUser } = useAuth()
@@ -69,6 +70,9 @@ export default function EditForm() {
     const handleOnChange_17 = (e) => {
         setFail(e.target.value);
     }
+    const createCancel = () => {
+        history.push('/');
+    }
     const history = useHistory();
     const createTodo = () => {
         var dateObj = new Date();
@@ -99,10 +103,10 @@ export default function EditForm() {
     }
 
     return (
-        <div style={{ columnWidth: 600, backgroundColor: '#62C6F2' }}>
-            <p style={{ textAlign: 'center', fontSize: 50 }}>Add an Entry here!</p>
+        <div id = "d">
+            <p id = "z">Add an Entry here!</p>
             <div>
-                <div style={{ height: 600, width: 800 }}>
+                <div id = "pl">
                     <form>
                         <div>
                             <div>
@@ -110,13 +114,13 @@ export default function EditForm() {
                                     <div>
                                         <label>Professor's First Name:</label>
                                         <div>
-                                            <input type="text" id="professorFirst1" class="professorFirst input" onChange={handleOnChange} name="professorFirst" required />
+                                            <input type="text" onChange={handleOnChange} name="professorFirst" required />
                                         </div>
                                     </div>
                                     <div class="field">
                                         <label for="professorLast1" class="label">Professor's Last Name:</label>
                                         <div class="control">
-                                            <input type="text" id="professorLast1" class="professorLast input" onChange={handleOnChange_2} name="professorLast" required />
+                                            <input type="text" onChange={handleOnChange_2} name="professorLast" required />
                                         </div>
                                     </div>
                                     <div class="field">
@@ -137,18 +141,18 @@ export default function EditForm() {
                                 <div class="rating">
                                     <h4 class="label">How would you rate them?</h4>
                                     <div class="ratingButtons">
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_4} id="rating1" name="rating" value="1" required />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_4} id="rating2" name="rating" value="2" />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_4} id="rating3" name="rating" value="3" />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_4} id="rating4" name="rating" value="4" />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_4} id="rating5" name="rating" value="5" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_4} id="rating1" name="rating" value="1" required />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_4} id="rating2" name="rating" value="2" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_4} id="rating3" name="rating" value="3" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_4} id="rating4" name="rating" value="4" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_4} id="rating5" name="rating" value="5" />
                                     </div>
                                     <div class="ratinglabels">
-                                        <label for="rating1" style={{ marginRight: 30, fontSize: 20 }}>1</label>
-                                        <label for="rating2" style={{ marginRight: 30, fontSize: 20 }}>2</label>
-                                        <label for="rating3" style={{ marginRight: 30, fontSize: 20 }}>3</label>
-                                        <label for="rating4" style={{ marginRight: 30, fontSize: 20 }}>4</label>
-                                        <label for="rating5" style={{ marginRight: 30, fontSize: 20 }}>5</label>
+                                        <label for="rating1" class="radioLabel">1</label>
+                                        <label for="rating2" class="radioLabel">2</label>
+                                        <label for="rating3" class="radioLabel">3</label>
+                                        <label for="rating4" class="radioLabel">4</label>
+                                        <label for="rating5" class="radioLabel">5</label>
                                     </div>
                                 </div>
                             </div>
@@ -158,18 +162,18 @@ export default function EditForm() {
                                 <div class="diff">
                                     <h4 class="label">How difficult was the class?</h4>
                                     <div class="diffButtons">
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_5} id="diff1" name="diff" value="1" required />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_5} id="diff2" name="diff" value="2" />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_5} id="diff3" name="diff" value="3" />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_5} id="diff4" name="diff" value="4" />
-                                        <input type="radio" style={{ marginRight: 27.75 }} onChange={handleOnChange_5} id="diff5" name="diff" value="5" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_5} id="diff1" name="diff" value="1" required />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_5} id="diff2" name="diff" value="2" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_5} id="diff3" name="diff" value="3" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_5} id="diff4" name="diff" value="4" />
+                                        <input type="radio" class="radioButton" onChange={handleOnChange_5} id="diff5" name="diff" value="5" />
                                     </div>
                                     <div class="difflabels">
-                                        <label for="diff1" style={{ marginRight: 30, fontSize: 20 }}>1</label>
-                                        <label for="diff2" style={{ marginRight: 30, fontSize: 20 }}>2</label>
-                                        <label for="diff3" style={{ marginRight: 30, fontSize: 20 }}>3</label>
-                                        <label for="diff4" style={{ marginRight: 30, fontSize: 20 }}>4</label>
-                                        <label for="diff5" style={{ marginRight: 30, fontSize: 20 }}>5</label>
+                                        <label for="diff1" class="radioLabel">1</label>
+                                        <label for="diff2" class="radioLabel">2</label>
+                                        <label for="diff3" class="radioLabel">3</label>
+                                        <label for="diff4" class="radioLabel">4</label>
+                                        <label for="diff5" class="radioLabel">5</label>
                                     </div>
                                 </div>
                             </div>
@@ -179,12 +183,12 @@ export default function EditForm() {
                                 <div class="retake">
                                     <h4 class="label">Would you take them again?</h4>
                                     <div class="retakeButtons">
-                                        <input type="radio" style={{ marginLeft: 7, marginRight: 35 }} onChange={handleOnChange_6} id="retakeYes" name="retake" value="Yes" required />
-                                        <input type="radio" style={{ marginLeft: 7, marginRight: 35 }} onChange={handleOnChange_6} id="retakeNo" name="retake" value="No" />
+                                        <input type="radio" class="radioButton2" onChange={handleOnChange_6} id="retakeYes" name="retake" value="Yes" required />
+                                        <input type="radio" class="radioButton2" onChange={handleOnChange_6} id="retakeNo" name="retake" value="No" />
                                     </div>
                                     <div class="retakelabels">
-                                        <label for="retakeYes" style={{ marginRight: 30, fontSize: 20 }}>Yes</label>
-                                        <label for="retakeNo" style={{ marginRight: 30, fontSize: 20 }}>No</label>
+                                        <label for="retakeYes" class="radioLabel">Yes</label>
+                                        <label for="retakeNo" class="radioLabel">No</label>
                                     </div>
                                 </div>
                             </div>
@@ -194,12 +198,12 @@ export default function EditForm() {
                                 <div class="book">
                                     <h4 class="label">Did the course require a textbook? <em class="has-text-grey-dark">(Optional)</em></h4>
                                     <div class="bookButtons">
-                                        <input type="radio" style={{ marginLeft: 7, marginRight: 35 }} onChange={handleOnChange_7} id="bookYes" name="book" value="Yes" />
-                                        <input type="radio" style={{ marginLeft: 7, marginRight: 35 }} onChange={handleOnChange_7} id="bookNo" name="book" value="No" />
+                                        <input type="radio" class="radioButton2" onChange={handleOnChange_7} id="bookYes" name="book" value="Yes" />
+                                        <input type="radio" class="radioButton2" onChange={handleOnChange_7} id="bookNo" name="book" value="No" />
                                     </div>
                                     <div class="bookLabels">
-                                        <label for="bookYes" style={{ marginRight: 30, fontSize: 20 }}>Yes</label>
-                                        <label for="bookNo" style={{ marginRight: 30, fontSize: 20 }}>No</label>
+                                        <label for="bookYes" class="radioLabel">Yes</label>
+                                        <label for="bookNo" class="radioLabel">No</label>
                                     </div>
                                 </div>
                             </div>
@@ -209,12 +213,12 @@ export default function EditForm() {
                                 <div class="attend">
                                     <h4 class="label">Was attendance required? <em class="has-text-grey-dark">(Optional)</em></h4>
                                     <div class="attendButtons">
-                                        <input type="radio" style={{ marginLeft: 7, marginRight: 35 }} onChange={handleOnChange_8} id="attendYes" name="attend" value="Yes" />
-                                        <input type="radio" style={{ marginLeft: 7, marginRight: 35 }} onChange={handleOnChange_8} id="attendNo" name="attend" value="No" />
+                                        <input type="radio" class="radioButton2" onChange={handleOnChange_8} id="attendYes" name="attend" value="Yes" />
+                                        <input type="radio" class="radioButton2" onChange={handleOnChange_8} id="attendNo" name="attend" value="No" />
                                     </div>
                                     <div class="attendLabels">
-                                        <label for="attendYes" style={{ marginRight: 30, fontSize: 20 }}>Yes</label>
-                                        <label for="attendNo" style={{ marginRight: 30, fontSize: 20 }}>No</label>
+                                        <label for="attendYes" class="radioLabel">Yes</label>
+                                        <label for="attendNo" class="radioLabel">No</label>
                                     </div>
                                 </div>
                             </div>
@@ -222,7 +226,7 @@ export default function EditForm() {
                         <div class="field">
                             <div class="control">
                                 <div class="grade">
-                                    <h4 class="label">What grade did you recieve? <em class="has-text-grey-dark">(Optional)</em></h4>
+                                    <h4 class="label">What grade did you recieve? <em>(Optional)</em></h4>
                                     <div class="control">
                                         <div class="select">
                                             <select name="grade" onChange={handleOnChange_9}>
@@ -253,27 +257,27 @@ export default function EditForm() {
                             <div class="control">
                                 <div class="attr">
                                     <h4 class="label">Which of these best describes the professor? <em class="has-text-grey-dark">(Optional)</em></h4>
-                                    <label for="respect" style={{ marginRight: 10 }}>
+                                    <label for="respect" class="checkbox">
                                         <input type="checkbox" class="attrCheckbox checkRespect" onChange={handleOnChange_12} id="respect" name="respect" value="Respect" />
                                 Respectful
                             </label>
-                                    <label for="engaging" style={{ marginRight: 10 }}>
+                                    <label for="engaging" class="checkbox">
                                         <input type="checkbox" class="attrCheckbox checkEngaging" onChange={handleOnChange_13} id="engaging" name="engaging" value="Engaging" />
                                 Engaging
                             </label>
-                                    <label for="caring" style={{ marginRight: 10 }}>
+                                    <label for="caring" class="checkbox">
                                         <input type="checkbox" class="attrCheckbox checkCaring" onChange={handleOnChange_14} id="caring" name="caring" value="Caring" />
                                 Caring
                             </label>
-                                    <label for="few" style={{ marginRight: 10 }}>
+                                    <label for="few" class="checkbox">
                                         <input type="checkbox" class="attrCheckbox checkFew" onChange={handleOnChange_15} id="few" name="few" value="Few" />
                                 Few Grades
                             </label>
-                                    <label for="paper" style={{ marginRight: 10 }}>
+                                    <label for="paper" class="checkbox">
                                         <input type="checkbox" class="attrCheckbox checkPaper" onChange={handleOnChange_16} id="paper" name="paper" value="Paper" />
                                 Paper Heavy
                             </label>
-                                    <label for="mandatory" style={{ marginRight: 10 }}>
+                                    <label for="mandatory" class="checkbox">
                                         <input type="checkbox" class="attrCheckbox checkMandatory" onChange={handleOnChange_17} id="mandatory" name="mandatory" value="Mandatory" />
                                 Attend or Fail
                             </label>
@@ -290,6 +294,7 @@ export default function EditForm() {
                         </div>
                         <div class="control">
                             <input type="submit" class="button is-light" value="Submit" onClick={createTodo} />
+                            <input type="submit" class="button is-light" value="Cancel" onClick={createCancel} />
                         </div>
                     </form>
                 </div>
